8b17d3114c9b835838d8d142e823d5028eccb5f2
Yifan Liu <2582395L@student.gla.ac.uk>
2021-07-29 09:16:18 +0100

    Chapter 4: Browser enter http://127.0.0.1:8000/static/images/rango.jpg. The test is successful.

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -15,6 +15,11 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
+
+# Create a variable called STATIC_DIR and point to static.
+# This results in the path <workspace>/tango_with_django_project/static/.
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
@@ -26,6 +31,8 @@ SECRET_KEY = '8mx#n4nm6&3be2kh2^@!&b$q*546jql+c@!x%kq45(0j(oy_bu'
 DEBUG = True
 
 ALLOWED_HOSTS = []
+
+
 
 
 # Application definition
@@ -55,7 +62,7 @@ ROOT_URLCONF = 'tango_with_django_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR, ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -67,6 +74,9 @@ TEMPLATES = [
         },
     },
 ]
+
+# we’re only going to be using a single location to store our project’s static files – the path defined in STATIC_DIR. 
+STATICFILES_DIRS = [STATIC_DIR, ]
 
 WSGI_APPLICATION = 'tango_with_django_project.wsgi.application'
 
@@ -119,3 +129,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
