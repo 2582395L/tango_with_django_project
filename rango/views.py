638ee4649c983d271639178498685b6286b84805
Yifan Liu <2582395L@student.gla.ac.uk>
2021-07-30 12:05:44 +0100

    Implemented user logout and control access functions

--- rango/views.py
+++ rango/views.py
@@ -8,9 +8,8 @@ from rango.forms import PageForm
 from django.urls import reverse
 from rango.forms import UserForm, UserProfileForm
 from django.contrib.auth import authenticate, login
-
-
-
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
 
 
 # Create your views here.
@@ -152,3 +151,12 @@ def user_login(request):
             return HttpResponse("Invalid login details supplied.")
     else:
         return render(request, 'rango/login.html')
+
+@login_required
+def restricted(request):
+    return HttpResponse("Since you're logged in, you can see this text!")
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return redirect(reverse('rango:index'))
