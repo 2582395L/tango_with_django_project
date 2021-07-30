07eb2eff8d48dd9a0aa88bd1164a49d1a1ad0032
Yifan Liu <2582395L@student.gla.ac.uk>
2021-07-30 04:56:33 +0100

    Chapter7: Adding a new category to Rango with our new form.

--- rango/views.py
+++ rango/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from rango.models import Category
 from rango.models import Page
+from rango.forms import CategoryForm
+from django.shortcuts import redirect
 
 
 # Create your views here.
@@ -49,3 +51,19 @@ def show_category(request, category_name_slug):
         context_dict['pages'] = None
 # Go render the response and return it to the client.
     return render(request, 'rango/category.html', context=context_dict)
+
+def add_category(request):
+    form = CategoryForm()
+
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+
+        if form.is_valid():
+# Save the new category to the database.
+            form.save(commit=True)
+
+            return redirect('/rango/')
+        else:
+            print(form.errors)
+
+    return render(request, 'rango/add_category.html', {'form': form})
